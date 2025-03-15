1ed2e79441793acb55e102768f2684c629772f0b
darkhorse-406 <csmoedl@gmail.com>
2025-03-15 10:38:36 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -25,7 +25,6 @@ def build_ics_urls(ics_url):
     ics_url_webcal = urlunparse(parsed_ics_url)
 
     parsed_google_url = list(urlparse(google_calendar_url_base))
-    parsed_google_url = list(urlparse(google_calendar_url_base))
     parsed_google_url[4] = dict(parse_qsl(parsed_google_url[4]))
     parsed_google_url[4]['cid'] = ics_url_webcal
     parsed_google_url[4] = urlencode(parsed_google_url[4])
@@ -57,62 +56,64 @@ def load_groupme_json(app, groupme_api_key, groupme_group_id):
 
 def groupme_json_to_ics(groupme_json, static_name=None):
     cal = Calendar()
-    cal['prodid'] = '-//Andrew Mussey//GroupMe-to-ICS 0.1//EN'
-    cal['version'] = '2.0'
-    cal['calscale'] = 'GREGORIAN'
-    cal['method'] = 'PUBLISH'
-    cal['x-wr-calname'] = 'GroupMe: {}'.format(current_app.groupme_calendar_name)
-    cal['x-wr-timezone'] = current_app.calendar_timezone
+    # Use extra to add custom iCalendar properties
+    cal.extra.append("PRODID:-//Andrew Mussey//GroupMe-to-ICS 0.1//EN")
+    cal.extra.append("VERSION:2.0")
+    cal.extra.append("CALSCALE:GREGORIAN")
+    cal.extra.append("METHOD:PUBLISH")
+    cal.extra.append(f"X-WR-CALNAME:GroupMe: {current_app.groupme_calendar_name}")
+    cal.extra.append(f"X-WR-TIMEZONE:{current_app.calendar_timezone}")
 
     for json_blob in groupme_json['response']['events']:
         if 'deleted_at' not in json_blob:
             event = Event()
-            event['uid'] = json_blob['event_id']
-            event.add('dtstart', dateutil.parser.parse(json_blob['start_at']))
+            event.uid = json_blob['event_id']
+            event.begin = dateutil.parser.parse(json_blob['start_at'])
             if json_blob.get('end_at'):
-                event.add('dtend', dateutil.parser.parse(json_blob['end_at']))
-            event['summary'] = json_blob['name']
-            event['description'] = json_blob.get('description', '')
+                event.end = dateutil.parser.parse(json_blob['end_at'])
+            event.name = json_blob['name']
+            event.description = json_blob.get('description', '')
             if json_blob.get('location'):
                 location = json_blob.get('location', {})
 
                 if json_blob.get('description'):
-                    event['description'] += '\n\n'
-                event['description'] += 'Location:\n'
+                    event.description += '\n\n'
+                event.description += 'Location:\n'
 
                 if location.get('name') and location.get('address'):
-                    event['location'] = "{}, {}".format(location.get('name'), location.get('address').strip().replace("\n", ", "))
-                    event['description'] += location.get('name')
-                    event['description'] += '\n'
-                    event['description'] += location.get('address')
+                    event.location = "{}, {}".format(location.get('name'), location.get('address').strip().replace("\n", ", "))
+                    event.description += location.get('name')
+                    event.description += '\n'
+                    event.description += location.get('address')
                 elif location.get('name'):
-                    event['location'] = location.get('name')
-                    event['description'] += location.get('name')
+                    event.location = location.get('name')
+                    event.description += location.get('name')
                 elif location.get('address'):
-                    event['location'] = location.get('address').strip().replace("\n", ", ")
-                    event['description'] += location.get('address')
+                    event.location = location.get('address').strip().replace("\n", ", ")
+                    event.description += location.get('address')
 
                 if location.get('lat') and location.get('lng'):
                     location_url = 'https://www.google.com/maps?q={},{}'.format(location.get('lat'), location.get('lng'))
-                    if not event.get('location'):
-                        event['location'] = location_url
+                    if not event.location:
+                        event.location = location_url
                     else:
-                        event['description'] += '\n'
-                    event['description'] += location_url
+                        event.description += '\n'
+                    event.description += location_url
 
             if json_blob.get('updated_at'):
-                event['last-modified'] = dateutil.parser.parse(json_blob.get('updated_at'))
-            cal.add_component(event)
+                event.last_modified = dateutil.parser.parse(json_blob.get('updated_at'))
+            cal.events.add(event)
 
-    return cal.to_ical()
+    return cal.serialize()
 
 def groupme_ics_error(error_text, static_name=None):
     cal = Calendar()
-    cal['prodid'] = '-//Andrew Mussey//GroupMe-to-ICS 0.1//EN'
-    cal['version'] = '2.0'
-    cal['calscale'] = 'GREGORIAN'
-    cal['method'] = 'PUBLISH'
-    cal['x-wr-calname'] = 'GroupMe: {} ({})'.format(current_app.groupme_calendar_name, error_text)
-    cal['x-wr-timezone'] = 'America/Los_Angeles'
+    # Use extra to add custom iCalendar properties
+    cal.extra.append("PRODID:-//Andrew Mussey//GroupMe-to-ICS 0.1//EN")
+    cal.extra.append("VERSION:2.0")
+    cal.extra.append("CALSCALE:GREGORIAN")
+    cal.extra.append("METHOD:PUBLISH")
+    cal.extra.append(f"X-WR-CALNAME:GroupMe: {current_app.groupme_calendar_name} ({error_text})")
+    cal.extra.append("X-WR-TIMEZONE:America/Chicago")
 
-    return cal.to_ical()
+    return cal.serialize()
